afe854ace93b4bc136cd3a6ef1942755f858183a
Mai Lam <mai.khanlam@vodafone.com>
2016-11-23 14:07:44 +1300

    show error if no uuid provided

--- tail-log.sh
+++ tail-log.sh
@@ -1,23 +1,23 @@
 #!/bin/bash
-# set -e
+set -e
 
-# normal=$(tput sgr0)
-# black=$(tput setaf 0)
-# red=$(tput setaf 1)
-# green=$(tput setaf 2)
-# yellow=$(tput setaf 3)
-# blue=$(tput setaf 4)
-# magenta=$(tput setaf 5)
-# cyan=$(tput setaf 6)
-# white=$(tput setaf 7)
+normal=$(tput sgr0)
+black=$(tput setaf 0)
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+blue=$(tput setaf 4)
+magenta=$(tput setaf 5)
+cyan=$(tput setaf 6)
+white=$(tput setaf 7)
 
-# if [[ $1 -eq 0 ]] ; then
-#   echo ""
-#   printf "%40s\n" "${red}ERROR :: No simulator UUID provided${normal}"
-#   printf "%40s\n" "Run [instruments -s devices] or go to [XCode > Window > Devices] to find simulator UUID"
+if [[ $1 -eq 0 ]] ; then
+  echo ""
+  printf "%40s\n" "${red}ERROR :: No simulator UUID provided${normal}"
+  printf "%40s\n" "Run [instruments -s devices] or go to [XCode > Window > Devices] to find simulator UUID"
 
-#   exit 1
-# fi
+  exit 1
+fi
 
 SIMULATOR_UUID=$1 
 SEARCH_KEY=$2
